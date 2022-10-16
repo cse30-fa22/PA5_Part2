02bb2d4eb54bb0344e32a43ecca31d231a2f6cb9
keithmuller <66692457+keithmuller@users.noreply.github.com>
2022-10-16 14:54:15 -0700

    Initial version

---
+++ wrtrow.h
@@ -0,0 +1,14 @@
+/* 
+ * Header file for PA4
+ * Author: Keith Muller
+ * the next two lines are called header guards
+ * we will cover this later in the quarter
+ */
+#ifndef WRTROW_H
+#define WRTROW_H
+
+ /*
+  * Function prototypes
+  */
+void wr_row(char **, int *, int, char);
+#endif
