1332e04e6d11c307a6386ae5eeec810b19053bc0
keithmuller <66692457+keithmuller@users.noreply.github.com>
2022-10-19 20:31:34 -0700

    Updated comment

--- wrtrow.c
+++ wrtrow.c
@@ -9,13 +9,13 @@
  *          output those columns specified in the output array (contents are
  *          index numbers of the input array)
  * args:
- *  ptab    points at array of pointers each element points at a data field
- *  coltab  points array of ints each element is an index in ptab in print order
+ *  ptable  points at array of pointers each element points at a data field
+ *  coltab  points array of ints each element is an index in ptable in print order
  *  outcols number of elements in array coltab
  *  outdelim the output field delimiter to use
  */
 void
-wrtrow(char **ptab, int *coltab, int outcols, char outdelim)
+wrtrow(char **ptable, int *coltab, int outcols, char outdelim)
 {
     /*
      * PA5part2 action: delete this comment
